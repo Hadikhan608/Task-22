eb663d81c7c1078aefc45cdc41ffcf4c7aea9924
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-06-08 13:36:00 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,7 @@
+//Task 22: Intentional error, if you havn't recieved an array index error
+//in one of your program yet, try to make one happen. change and index in one of your program
+//produce an index error, make sure you correct the error before closing the program
+
+const weekdays:string[]= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//console.log(weekdays[7]);  //error is showing
+console.log(weekdays[6]);
